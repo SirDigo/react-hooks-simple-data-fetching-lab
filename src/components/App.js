8983d127794a5e7f9f5bc24f01108932a1d925c5
Caleb Rodriguez <sirdigo6@gmail.com>
2021-12-22 19:53:28 -0500

    commit tree 16185d6f4f812e0703788d2344db18c7d47d4021

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import { useEffect, useState } from "react";
+
+function App(){
+
+    const [photo, setPhoto] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => setPhoto(data.message))
+    }, [])
+
+    console.log(photo)
+
+    const newPhoto = photo === "" ? <p>Loading...</p> : <img src={photo} alt="A Random Dog"></img>
+
+    return (
+        newPhoto
+    )
+}
+
+export default App;
